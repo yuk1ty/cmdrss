77f6e6964fad0608986d1d4c21bdb36d69bd8c06
Yuki <yuki.mul.tiplus@gmail.com>
2018-03-21 21:57:32 +0900

    Add serde and shows links by json

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate serde_derive;
 extern crate rss;
+extern crate serde;
+extern crate serde_json;
 
 use clap::{App, Arg};
 use std::str::FromStr;
 use rss::Channel;
+
+fn main() {
+    let arg_matches = App::new("cmdrss")
+        .version("1.0")
+        .about("A RSS reader runs on command line.")
+        .author("Yuki Toyoda")
+        .arg(
+            Arg::from_usage("<feeds> 'みたいサイト'")
+                .possible_values(&["hacker", "reddit", "hatena"]),
+        )
+        .get_matches();
+
+    let ty = value_t!(arg_matches, "feeds", Rss).unwrap_or_else(|e| e.exit());
+
+    match ty {
+        Rss::HackerNews(url) => create_list(&url)
+            .iter()
+            .for_each(|item| println!("{}", serde_json::to_string(&item).unwrap())),
+        Rss::Reddit(url) => create_list(&url)
+            .iter()
+            .for_each(|item| println!("{}", serde_json::to_string(&item).unwrap())),
+        Rss::HatenaBookMark(url) => create_list(&url)
+            .iter()
+            .for_each(|item| println!("{}", serde_json::to_string(&item).unwrap())),
+    };
+}
 
 enum Rss {
     HackerNews(String),
@@ -29,31 +59,21 @@ impl FromStr for Rss {
     }
 }
 
-fn main() {
-    let arg_matches = App::new("cmdrss")
-        .version("1.0")
-        .about("A RSS reader runs on command line.")
-        .author("Yuki Toyoda")
-        .arg(
-            Arg::from_usage("<feeds> 'みたいサイト'")
-                .possible_values(&["hacker", "reddit", "hatena"]),
-        )
-        .get_matches();
+#[derive(Serialize, Deserialize, Debug)]
+struct FeedItem {
+    title: String,
+    link: String,
+}
 
-    let ty = value_t!(arg_matches, "feeds", Rss).unwrap_or_else(|e| e.exit());
-
-    match ty {
-        Rss::HackerNews(url) => {
-            let channel = Channel::from_url(&url).unwrap();
-            println!("{}", channel.to_string());
-        }
-        Rss::Reddit(url) => {
-            let channel = Channel::from_url(&url).unwrap();
-            println!("{}", channel.to_string());
-        }
-        Rss::HatenaBookMark(url) => {
-            let channel = Channel::from_url(&url).unwrap();
-            println!("{}", channel.to_string());
-        }
-    }
+fn create_list(url: &str) -> Vec<FeedItem> {
+    let channel = Channel::from_url(url).unwrap();
+    let items: Vec<FeedItem> = channel
+        .items()
+        .iter()
+        .map(|item| FeedItem {
+            title: item.title().unwrap().to_string(),
+            link: item.link().unwrap().to_string(),
+        })
+        .collect();
+    items
 }
